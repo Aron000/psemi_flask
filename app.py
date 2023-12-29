39ee6488b97e3723a24d12e4c05babbec8e0c8d0
Aron000 <akiizukuzuki@yahoo.co.jp>
2023-12-29 19:26:09 +0900

    更新機能の作成

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 db = SQLAlchemy(app)
@@ -14,32 +12,57 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     details = db.Column(db.String(100))
 
-
+@app.route("/")
+def index():
+    tasks = Todo.query.all()
+    return render_template("index.html", tasks=tasks)
 
 @app.route("/create", methods=["POST"])
 def create():
-        title = request.form.get("title")
-        details = request.form.get("details")
-        new_task = Todo(title=title, details=details)
+    title = request.form.get("title")
+    details = request.form.get("details")
+    new_task = Todo(title=title, details=details)
 
-        db.session.add(new_task)
-        db.session.commit()
-        return redirect("/")
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect("/")
 
 @app.route('/delete/<int:id>')
 def delete(id):
     delete_task = Todo.query.get(id)
+
     db.session.delete(delete_task)
     db.session.commit()
     return redirect('/')
 
-
-
-@app.route("/")
-def index():
-     tasks = Todo.query.all()
-     return render_template("index.html", tasks=tasks)
+## TODO
+## /update/1のような遷移先を作成する
+@app.route('/update/<int:id>',methods=["POST", "GET"])
+## /update/1　に来たときの処理を実装する
+def update(id):
+    ## idをもとにデータベースからタスクを取得する
+    update_task = Todo.query.get(id)
+    
+    ## GETのときはupdate.htmlを表示する
+    ## update.htmlには元々のタスクの情報を表示したい
+    if request.method == 'GET':
+        return render_template("update.html", update_task = update_task)
 
     
+    ## POSTのときはデータベースを更新して/にリダイレクトする
+    ## データベースを更新するときは、タスクを取得→取得したタスクの内容を更新→保存する
+    elif request.method == 'POST':
+        title = request.form.get("title")
+        details = request.form.get("details")
+        update_task.title = title
+        update_task.details = details
+
+        db.session.add(update_task)
+        db.session.commit()
+        return redirect("/")
+
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
